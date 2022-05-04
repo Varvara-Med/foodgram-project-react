ee46fe2bc0c410d2aa98d5db59c5aed93fad91a5
Варвара <varvara.kireeva@gmail.com>
2022-05-04 21:33:01 +0200

    admin zone and superuser created

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+from .models import (Favorite, Ingredient,
+                     Recipe, Tag)
+
+
+class UserAdmin(admin.ModelAdmin):
+    """
+    Параметры админ зоны пользователя.
+    """
+    list_display = ('username', 'email', 'id')
+    search_fields = ('username', 'email')
+    empty_value_display = '-пусто-'
+    list_filter = ('username', 'email')
+
+
+admin.site.register(User, UserAdmin)
+
